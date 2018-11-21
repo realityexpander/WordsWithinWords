60b7a66cbe185e4ca73f73ff661bec0d1b1ffa9e
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-21 11:50:33 -0600

    Update nestedWordCount.js

--- nestedWordCount.js
+++ nestedWordCount.js
@@ -1,5 +1,15 @@
 function nestedWordCount (wordList) {
 
 
+// gray -> ray, gray
+// grays -> gray, grays, ray, rays 
+// ray -> ray
+// rays -> ray, rays
+// strays -> ray, rays
+
+// For each word
+  // check aaginst all other words
+    // does this word contain the other word?
+      // yes -> increment count for this word
 
 }
